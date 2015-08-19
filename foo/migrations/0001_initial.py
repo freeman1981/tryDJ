ff8bf7fc09b772727d2161f2f2792824a716ac14
__freeman__ <alexander.i.kamenev@gmail.com>
2015-08-19 17:20:42 +0300

    move on tutorial

---
+++ foo/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Car',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=5)),
+                ('photo', models.ImageField(upload_to='foo')),
+            ],
+        ),
+    ]
