bbeb8caff06ba788588e1e1b045850ad7c1bed83
Alexander <alexander.i.kamenev@mail.com>
2015-08-24 23:17:46 +0300

    go through tutorial

--- mdls/urls.py
+++ mdls/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from .views import QuestionListView
 
 urlpatterns = [
+    url(r'^$', QuestionListView.as_view(), name='start-quiz'),
     url(r'^(\d+)$', QuestionListView.as_view(), name='question_list'),
 ]
--- mdls/views.py
+++ mdls/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
-from django.shortcuts import get_list_or_404
+from django.views.generic.base import RedirectView
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 from .models import Question, Answer
 
@@ -10,3 +11,13 @@ class QuestionListView(ListView):
 
     def get_queryset(self):
         return get_list_or_404(Question, pk=self.args[0])
+
+
+class StartQuiz(RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'question_list'
+
+    def get_redirect_url(self, *args, **kwargs):
+        get_object_or_404(Question, pk=1)
+        return super(StartQuiz, self).get_redirect_url(*args, **kwargs)
