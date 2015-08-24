4a76a9eed0b7bac0c516b17b94e3aacd9cd83136
__freeman__ <alexander.i.kamenev@gmail.com>
2015-08-24 11:48:03 +0300

    move on tutorial

--- foo/urls.py
+++ foo/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import CarList
 
 urlpatterns = [
-    url(r'^$', CarList.as_view(), name='car_list'),
+    url(r'^([\w-]+)$', CarList.as_view(), name='car_list'),
 ]
--- foo/views.py
+++ foo/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import ListView
+from django.shortcuts import get_list_or_404
+
 from .models import Car
 
 
 class CarList(ListView):
-    model = Car
     context_object_name = 'list_of_cars'
+    template_name = 'foo/car_list.html'
+
+    def get_queryset(self):
+        return get_list_or_404(Car, name=self.args[0])
