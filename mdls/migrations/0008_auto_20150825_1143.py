14ece264d9958e5049702aa5bc19ae7f241f677d
__freeman__ <alexander.i.kamenev@gmail.com>
2015-08-25 11:54:02 +0300

    move on tutorial

---
+++ mdls/migrations/0008_auto_20150825_1143.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mdls', '0007_auto_20150825_1118'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='answer',
+            name='to_q',
+            field=models.ForeignKey(to='mdls.Question', blank=True),
+        ),
+    ]
